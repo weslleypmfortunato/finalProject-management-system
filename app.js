f2851b217f81ac78a66aba7b9480e33250f7afd5
weslleypmfortunato <111170704+weslleypmfortunato@users.noreply.github.com>
2023-02-26 04:11:25 -0800

    updated cors rules

--- app.js
+++ app.js
@@ -8,9 +8,12 @@ import employeesListRouter from "./routes/employeesList.routes.js"
 const app = express()
 connectDB()
 
-app.use(cors({
-  origin: ['http://localhost:3000', process.env.REACT_URL]
-}))
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  app.use(cors())
+  next()
+})
+
 app.use(express.json())
 
 app.use(authRouter)
